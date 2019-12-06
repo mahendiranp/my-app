7e740cf10a51ce70b60b9cdb0c5b8bdf4629335f
Mahendran <info@mahendran.net>
2019-12-07 05:00:15 +0530

    build config

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
+    path: path.resolve(__dirname, "build/"),
     filename: "[name].bundle.js"
   },
   devServer: {
